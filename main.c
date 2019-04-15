a052b4fe8db1d079233479e91baf3cf9abb97702
dddaron <49554142+dddaron@users.noreply.github.com>
2019-04-15 19:17:39 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
-#include "usbtest.c"
-#include "link.c"
+#include "usbtest.h"
+#include "link.h"
 
-int main()
+int main(int argc,char *argv[])
 {
-  
+	jpeg_linklist *jpg = NULL;
+	jpg = create_list("NULL");
+
+	if(jpg == NULL)
+	{
+		printf("create jpeg list fail\n");
+		return -1;
+	}
+
+
+	if(argc == 1)
+		dir_order(".",jpg);
+	else
+		dir_order(argv[1],jpg);
+
+
+	show_list(jpg);
+				
+	return 0;
 }
